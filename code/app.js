fdab75abe1151820f55445fd2ce06f3053cb9ce8
DjhonataEverton <djhonataeverton@gmail.com>
2023-01-07 22:26:12 -0300

    teste get

--- code/app.js
+++ code/app.js
@@ -2,6 +2,9 @@ const express = require("express")
 
 const app = express()
 
+app.get('/', (req, res) =>{
+    return res.send("<h1>Teste</h1>")
+})
 
 app.listen(3000, ()=> {
     console.log("ta prestando")
