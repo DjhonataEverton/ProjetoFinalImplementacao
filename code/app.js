32b909b256cba4a2edaa6755c2b3221df983a264
DjhonataEverton <djhonataeverton@gmail.com>
2023-01-11 20:11:32 -0300

    create pages

--- code/app.js
+++ code/app.js
@@ -1,7 +1,7 @@
 const express = require("express")
-const cliente = require("../code/controllers/clientController")
+//const cliente = require("../code/controllers/clientController")
 const path = require("path")
-//const cliente = require("../code/models/clientModel")
+const clientes = require("../code/models/clientModel")
 const app = express()
 
 //app.use(express.json)
@@ -12,15 +12,23 @@ app.use(express.static(path.join(__dirname, "public")))
 app.get("/home", (req,res) =>{
     res.render('home')
 })
-
 app.get("/login", (req,res) =>{
     res.render('login')
 })
 app.get("/cadastro", (req,res) =>{
     res.render('cadastro')
 })
+app.get("/userdashbord", (req, res) =>{ 
+    res.render("clientDashbord")
+})
 app.get("/cliente", (req, res) =>{
-    res.send(cliente.listClients())
+    res.send(clientes.list_clients())
+})
+
+app.post("/cadpost", (req, res) =>{
+    const data = req.body
+    clientes.create_client(data)
+    res.send('<script>alert("cadastrado com sucesso")</script>')   
 })
 
 app.get('/', (req, res) =>{
